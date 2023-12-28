b38185943b590594440f6a70bb2538e55f6b3077
Trollkopf <max.serratosa@gmail.com>
2023-12-28 16:19:19 +0100

    specific chat created, added database information

--- controllers/chatcontroller.php
+++ controllers/chatcontroller.php
@@ -20,6 +20,34 @@ public static function read_whole_chat(){
 
 }
 
+public static function read_specific_chat($userid, $chatreceiver){
+
+	$result = [];
+
+	$conn = DB::conn();
+    $sql="SELECT c.sender AS senderID, 
+			     c.receiver AS receiverID, 
+				 r.name AS sender, 
+				 rr.name AS receiver, c
+				 .message AS message 
+			FROM `specific_chat` c 
+			LEFT JOIN `register` r ON c.sender = r.id 
+			LEFT JOIN `register` rr ON c.receiver = rr.id 
+			WHERE (c.sender = :userid AND c.receiver = :chatreceiver) OR (c.sender = :chatreceiver AND c.receiver = :userid)";
+
+	$query= $conn->prepare($sql);
+	$query->bindParam(":userid", $userid);
+	$query->bindParam(":chatreceiver", $chatreceiver);
+	$query->execute();
+	while($row = $query->fetch(PDO::FETCH_OBJ)){
+		array_push($result, $row);
+	}
+	$query->closecursor();
+	$conn= null;
+	return $result;
+
+}
+
 function send_message($id, $msg){
     
 	$conn = DB::conn();
---
+++ specific_chat.php
@@ -0,0 +1,102 @@
+<?php 
+	session_start();
+		include_once "./layouts/header2.php"; 
+		include_once "./controllers/chatcontroller.php";
+		include_once('./database/DB.php');
+
+		$userid = $_SESSION['ID'];
+		$receiverid = $_POST['receiver_id'];
+
+			$query = ChatController::read_specific_chat($userid, $receiverid);
+		?>
+		
+	<style>
+		#display_chat{
+			height:500px;
+			overflow-anchor: none;
+
+		}
+
+		#anchor{
+			overflow-anchor: auto;
+			height: 1px;
+		}
+	</style>
+	
+	<script>
+		$(document).ready(function(){
+		// Set trigger and container variables
+			var trigger = $('.container #display_chat '),
+				container = $('#content');
+			// Fire on click
+			trigger.on('click', function(){
+				// Set $this for re-use. Set target from data attribute
+				var $this = $(this),
+				target = $this.data('target');       
+				// Load target page into container
+				container.load(target + '.php');
+				// Stop normal link behavior
+				return false;
+			});
+		});
+	</script>
+	
+	<div class="container bg-dark mt-3 p-3 rounded-lg">
+		<h2 class="text-light d-flex justify-content-center">Bienvenid@ &nbsp; <span style="color:#dd7ff3;"><?php echo $_SESSION['name']; ?> </span>!</h2>
+		<h3 class="text-white d-flex justify-content-center">Chat general</h3>
+		<div class="overflow-auto bg-dark mb-2" id ="display_chat">
+	
+			<?php if(count($query) >0):
+				foreach($query as $q):
+				
+					if($_SESSION['ID'] == $q->senderID): ?>
+						<div class="d-flex justify-content-end mx-3 mb-1">
+							<p class="bg-warning rounded-lg p-3">
+								<span class="font-weight-bold"><?php echo $q->sender; ?> :</span>
+								<?php echo $q->message; ?>
+							</p>
+						</div>
+					<?php else: ?>
+						<div class="d-flex justify-content-start mx-3 mb-1">
+							<p class="bg-info rounded-lg p-3">
+								<span class="font-weight-bold"><?php echo $q->receiver; ?> :</span>
+								<?php echo $q->message; ?>
+							</p>
+						</div>
+					<?php endif; 
+				endforeach; ?>
+	
+			<?php else: ?>
+				<div class="message">
+					<p class="bg-warning rounded-lg p-3 d-flex justify-content-center"> No hay ninguna conversación previa.</p>
+				</div>
+			<?php endif ?>
+
+			<div id="chat_anchor"></div>
+		</div>
+		
+		<form class="form-horizontal" method="post" action="./router/router.php">
+			<div class="form-group d-flex justify-items-center p-2">
+			<div class="col-sm-10">        
+				<textarea name="msg" class="form-control" id="message" placeholder="Ingresa tu mensaje aquí..."></textarea>
+			</div>  
+			<div class="col-sm-2 p-2">
+				<button type="submit" name="new_msg" value="new_msg" class="btn btn-primary form-control" >Enviar</button>
+			</div>
+			</div>
+		</form>
+	</div>
+
+	<script>
+		// Pin the chat scroll to bottom
+		
+		document.getElementById("chat_anchor").scrollIntoView({block: "end"});
+
+	</script>
+	
+	
+	</body>
+	</html>
+		
+
+		
